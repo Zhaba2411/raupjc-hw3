e51daef73268c05d56bec2245749d2fb11460662
Janko Žabojec <jz49497@fer.hr>
2017-12-12 21:24:13 +0100

    Task2 pocetak

---
+++ RaupjccHw3/Task2/Controllers/TodoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Task1;
+
+namespace Task2.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly ITodoRepository _repository;
+        public TodoController(ITodoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
